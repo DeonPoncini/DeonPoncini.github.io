db6c041decc24e4f13bbf1fdf526b3a289752c7a
Deon Poncini <deonponcini@gmail.com>
2020-02-22 10:19:19 -0800

    Initial website release for wowsim

---
+++ precache-manifest.409a24d45c99cbb875bface0db08cf2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88b900a75e5f1fc40585b91aa6c0cec6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1bb3f54cb92a2d24799",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c5562a645951f8bae4be",
+    "url": "/static/js/2.75b617b2.chunk.js"
+  },
+  {
+    "revision": "a52d65c95a2d7ffcb9ff212c4d38dff4",
+    "url": "/static/js/2.75b617b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1bb3f54cb92a2d24799",
+    "url": "/static/js/main.5b77ccf0.chunk.js"
+  },
+  {
+    "revision": "2efda3639dd2ca3bf2fb",
+    "url": "/static/js/runtime-main.539b6b82.js"
+  }
+]);
